cd9bd692971d850197e04a542624055f68c4f354
Adrian Gonzalez <adriangonzalezsanchez1996@gmail.com>
2021-05-05 23:51:07 -0400

    Refactor index initialization

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from dependencies.startup import get_disease_service
-from typing import Dict, Tuple
 from fastapi import FastAPI
 import loaders
 from controllers.disease_controller import disease_controller
 from controllers.search_controller import search_controller
-from services.search_service import index
-from math import log10
+from services.search_service import Index, index
 from colorama import Fore
 import uvicorn
 import config.settings as conf
@@ -21,49 +18,7 @@ async def make_index():
     """
     Create the index for search engine.
     """
-    disease_service = get_disease_service()
-    diseases = disease_service.get_all()
-    freq: Dict[Tuple[str, int], int] = {}
-    idf: Dict[str, float] = {}
-    system_terms: Dict[str, int] = {}
-    tf: Dict[Tuple[str, int], float] = {}
-
-    # Compute tf frequencies
-    for disease in diseases:
-        terms = disease.get_terms()
-        unique_terms = set(terms)
-        for term in unique_terms:
-            freq[(term, disease.id)] = len(list(filter(lambda x: x == term, terms)))
-            try:
-                system_terms[term] += 1
-            except KeyError:
-                system_terms[term] = 1
-
-    # Compute TFij
-    for (term, doc), frequency in freq.items():
-        # Get the maximum frequency of all terms in doc
-        max_freq = max(
-            map(
-                lambda tfd: tfd[1],
-                filter(lambda tfd: tfd[0][1] == doc, freq.items()),
-            )
-        )
-        tf[term, doc] = frequency / max_freq
-
-    # Compute idf
-    N = len(list(diseases))
-    for term, ni in system_terms.items():
-        try:
-            idf[term] = log10(N / ni)
-        except ZeroDivisionError:
-            idf[term] = 0
-
-    # Compute weigths for each (document, term) pair
-    for (term, doc), tfij in tf.items():
-        index.weight_function[term, doc] = tfij * idf[term]
-
-    index.system_terms = system_terms.copy()
-    index.total_documents = N
+    Index.initialize(index)
     print(f"{Fore.GREEN}INFO{Fore.RESET}:     Index created")
 
 
--- services/search_service.py
+++ services/search_service.py
@@ -1,3 +1,4 @@
+from dependencies.startup import get_disease_service
 from dtos.query_dtos import QueryTerms
 from services.disease_service import DiseaseService
 from typing import Dict, List, Tuple
@@ -12,6 +13,52 @@ class Index:
         self.weight_function: Dict[Tuple[str, int], float] = {}
         self.system_terms: Dict[str, int] = {}
         self.total_documents: int = 0
+
+    @staticmethod
+    def initialize(index: "Index"):
+        disease_service = get_disease_service()
+        diseases = disease_service.get_all()
+        freq: Dict[Tuple[str, int], int] = {}
+        idf: Dict[str, float] = {}
+        system_terms: Dict[str, int] = {}
+        tf: Dict[Tuple[str, int], float] = {}
+
+        # Compute tf frequencies
+        for disease in diseases:
+            terms = disease.get_terms()
+            unique_terms = set(terms)
+            for term in unique_terms:
+                freq[(term, disease.id)] = len(list(filter(lambda x: x == term, terms)))
+                try:
+                    system_terms[term] += 1
+                except KeyError:
+                    system_terms[term] = 1
+
+        # Compute TFij
+        for (term, doc), frequency in freq.items():
+            # Get the maximum frequency of all terms in doc
+            max_freq = max(
+                map(
+                    lambda tfd: tfd[1],
+                    filter(lambda tfd: tfd[0][1] == doc, freq.items()),
+                )
+            )
+            tf[term, doc] = frequency / max_freq
+
+        # Compute idf
+        N = len(list(diseases))
+        for term, ni in system_terms.items():
+            try:
+                idf[term] = log10(N / ni)
+            except ZeroDivisionError:
+                idf[term] = 0
+
+        # Compute weigths for each (document, term) pair
+        for (term, doc), tfij in tf.items():
+            index.weight_function[term, doc] = tfij * idf[term]
+
+        index.system_terms = system_terms.copy()
+        index.total_documents = N
 
     def compute_query_vector(self, query: List[str], alpha=0.5) -> Dict[str, float]:
         result: Dict[str, float] = {}
